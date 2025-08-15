af5136d989b650c0804b6ad8ec9acb7659173d05
Galych-Yuri <59996176+Galych-Yuri@users.noreply.github.com>
2025-08-15 18:03:57 +0300

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,90 @@
+CONSTANTS = {
+    "FOOD_WATER": (
+        "Misc. General Merchandise",
+        "Grocery Stores, Supermarkets",
+        "Bakeries",
+        "Misc. Food Stores – Convenience Stores and Specialty Markets",
+        "Cigar Stores and Stands",
+        "News Dealers and Newsstands",
+        "Candy, Nut, and Confectionery Stores",
+        "Package Stores – Beer, Wine, and Liquor",
+        "Meat Provisioners – Freezer and Locker",
+    ),
+
+    "GOVERMENT": ("Tax Payments",),
+
+    "COMUNALKA": ("Electric, Gas, Sanitary and Water Utilities",),
+
+    "TELECOM": (
+        "Fax services, Telecommunication Services",
+        "Computer Network Services",
+    ),
+
+    "TRANSFER_TO_CARD": ("Money Orders – Wire Transfer",),
+
+    "CHEMISTRY": ("Cosmetic Stores",),
+
+    "PHARMACIES": ("Drug Stores and Pharmacies",),
+
+    "DOCTORS": ("Dentists and Orthodontists",),
+
+    "CARS_GASOLINE": (
+        "Service Stations ( with or without ancillary services)",
+        "Service Stations (with or without ancillary services)",
+        "Service Stations",
+        "Taxicabs and Limousines",
+    ),
+
+    "BOOKS": ("Book Stores",),
+
+    "YOGA": (),
+
+    "PHYSIOTHERAPY": (
+        "Membership Clubs (Sports, Recreation, Athletic), Country Clubs, and Private Golf Courses",
+    ),
+
+    "GARDEN": (
+        "Florists",
+        "Nurseries – Lawn and Garden Supply Store",
+        "Horticultural Services, Landscaping Services",
+    ),
+
+    "RESTAURANTS": ("Eating places and Restaurants",
+                    "Fast Food Restaurants",),
+
+    "ENTERTAINMENT": ("Розваги",),
+
+    "CLOTHING_FOOTWEAR": ("Sporting Goods Stores",),
+
+    "HAIRDRESSER": ("Перукар",),
+
+    "CURRENT_REPAIRS": (
+        "Home Supply Warehouse Stores",
+        "Lumber and Building Materials Stores",
+        "Telecommunications Equipment including telephone sales",
+    ),
+
+    "SUBSCRIPTIONS": (
+        "Computer Software Stores",
+        "Digital Goods: Large Digital Goods Merchant",
+    ),
+
+    "DR_HOLIDAYS": (),
+
+    "DEPOSITS": (),
+
+    "ALIEXPRESS": (),
+
+    "UNLISTED": (
+        "Financial Institutions – Merchandise and Services",
+        "Не має збігу по \"MCC\"",
+        "Not Elsewhere Classified",
+        "Business Services, Not Elsewhere Classified",
+        "Professional Services (Not Elsewhere Defined)",
+        "Professional Services ( Not Elsewhere Defined)",
+    ),
+
+    "UNKNOWN": (),
+
+    "INCOMING": ()
+}
